91ef5b2af0fcca0a6fa51cd1aa8d2fd39620f0d3
Cyborgnetical <Cyborgnetical@outlook.com>
2024-05-13 22:14:47 -0700

    got a working req and post

--- index.ts
+++ index.ts
@@ -1,16 +1,37 @@
 import express from "express";
+import { MongoClient } from "mongodb";
+
+
+
+const Client = new MongoClient(process.env.MONGODB_URI!).db("Omega_DB")
 
 const app = express();
 const port = 8080 || process.env.PORT;
 
 
 
-
+app.use(express.json())
 
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+
+app.get("/login",(req,res)=>{
+  res.send("Login <br><hr> <input>Username<br> <input>Password<br><hr><button>Submit</button>")
+})
+
+interface Login{
+  username:string;
+  password:string;
+}
+
+app.post("/login",(req,res)=>{
+  let Cridentials = req.body
+  console.log(Cridentials)
+  console.log("recieved")
+  res.status(300).send()
+})
 
 
 
