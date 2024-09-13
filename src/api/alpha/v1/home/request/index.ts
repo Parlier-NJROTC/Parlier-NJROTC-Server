cd51fdac5b4f860d8eb5067b1fb19bed3d2a464b
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2024-09-12 21:31:16 -0700

    simple ribbon request, time to fix my schedule

--- src/api/alpha/v1/home/request/index.ts
+++ src/api/alpha/v1/home/request/index.ts
@@ -1,15 +1,31 @@
 // route: /home/request/
 
 import express, { type Request, type Response, type NextFunction } from 'express';
+import mongoose from "mongoose";
 
+const Schema = mongoose.Schema;
 const router = express.Router()
+
+
+router.get("/ribbon",(req,res)=>{
+    res.status(200).send("this is getting annoying")
+
+})
+
+// ribbons
+const RibbonSchema = new Schema({
+    name:String
+})
+const RibbonRequest = mongoose.model("Ribbon_Request",RibbonSchema,"Ribbon_Requests")
+
 
 router.post("/ribbon/:ribbon",(req,res)=>{
     console.log(`recieved request for: ${req.params.ribbon}`)
-
-})
-router.post("/ribbon",()=>{
-    console.log("requesedt")
+    let request = new RibbonRequest({
+        name:req.params.ribbon
+    })
+    request.save()
+    res.status(200).send("test done, check database, ye lazy dingus")
 })
 
 
