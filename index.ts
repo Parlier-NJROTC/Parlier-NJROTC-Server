4fd4e6028659d8e2b56b942ae2a96b2467ac0a36
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2025-01-27 12:06:59 -0800

    changin
    ye

--- index.ts
+++ index.ts
@@ -1,41 +1,21 @@
-import express from "express"
-import session from "express-session"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import mongoose from "mongoose"
-
-// api routes
-import api from "./src/api"
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import multer from "multer";
 
 const PORT = process.env.port || 8080
 const app = express()
+const upload = multer({dest: "/temp"})
 
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_DB_URI as string)
-
-
-
-app.use("/api",api)
-
-app.get("/",express.static('public'))
-
-/*
-// Session Items
-app.use(cookieParser("ChangeBeforePushingToDevelopment"))
-app.use(session({
-    secret:"ChangeBeforePushingToDevelopment",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: false,
-        maxAge:1000 * 60 * 60 // An hour
-    }
-}))
-*/
+//mongoose.connect(process.env.MONGO_DB_URI as string)
 
 app.get("/",(req,res)=>{
     res.status(200).send("hello world")
+})
+app.post("/uploadtest",upload.single("picture"),(req,res,next)=>{
+// now im gonna ask to become class lead in 3 2 hold on now
 })
 
 app.listen(PORT,()=>{
