deea0becf17422c8e266d514698ef89ef78aa36a
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2025-04-21 23:46:10 -0700

    my brain calmed down

---
+++ src/api/dashboard/auth/login/index.ts
@@ -0,0 +1,48 @@
+import express, { type Request, type Response, type NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+import { Login, type LoginSchema } from "../../users";
+
+const Router = express.Router();
+const SECRET_KEY = process.env.JWT_SECRET_KEY!;
+
+Router.post("/", async (req: Request, res: Response) => {
+    console.log("received login request" + req.body);
+
+    let data: LoginSchema = req.body;
+    let login = await Login.findOne({ username: data.username });
+
+    if (!login) {
+        console.log("failed, no user found");
+        res.status(404).json({
+            success: false,
+            message: "Incorrect Password Or Username",
+            token: ""
+        });
+        return;
+    }
+
+    if (login.password !== data.password) {
+        console.log("failed, wrong password");
+        res.status(401).json({
+            success: false,
+            message: "Incorrect Password Or Username",
+            token: ""
+        });
+        return;
+    }
+
+    const Token = jwt.sign(
+        { username: data.username, id: login.userId },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+    );
+    res.status(200).json({
+        success: true,
+        message: "Successfully Logged In",
+        token: Token
+    });
+    return;
+});
+
+export default Router;
---
+++ src/api/dashboard/auth/signup/index.ts
@@ -0,0 +1,19 @@
+import express, { type Request, type Response, type NextFunction } from "express";
+
+// import subroute1 from "./route1"
+// import subroute2 from "./route2"
+// import subroute3 from "./route3"
+
+
+
+const Router = express.Router();
+
+// Router.use("/subroute1",subroute1);
+// Router.use("/subroute2",subroute2);
+// Router.use("/subroute3",subroute3);
+
+
+
+
+
+export default Router;
