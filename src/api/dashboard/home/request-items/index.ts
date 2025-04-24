60ee2dd5afe34b637f129f04875845ed4c37f033
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2025-04-23 22:33:35 -0700

    tired

--- src/api/dashboard/home/request-items/index.ts
+++ src/api/dashboard/home/request-items/index.ts
@@ -2,15 +2,25 @@
 /*
 This route is special because of regular JSON data it accepts formData
 
+This is to allow pictures to be uploaded and processed easier
 */
 
-import express from "express";
+import express, { type Request, type Response, type NextFunction } from "express";
 import multer from "multer";
 
 
 const Router = express.Router();
 
+const multerUploads = multer({ dest: '/temp/uploads/' });
 
+Router.post("/", async (req:Request,res:Response)=>{
+    if(req.file){
+        console.log("there be file")
+    }else{
+        console.log("nothin")
+    }
+
+})
 
 
 
