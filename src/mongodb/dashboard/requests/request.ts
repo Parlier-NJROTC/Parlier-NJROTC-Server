52821ed4e22a36d3782c738b8fa910b30e37a801
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2025-05-27 22:08:46 -0700

    wizarding

--- src/mongodb/dashboard/requests/request.ts
+++ src/mongodb/dashboard/requests/request.ts
@@ -1,12 +1,28 @@
+import type { TupleType } from "typescript";
 import DashboardDB from "../database-setup";
 import { Schema } from "mongoose";
 
 interface RequestSchema{
-
+    
 }
+
+// suggested by phind, looked it up and reasearched it for 10 mins
+// wth did I miss a core concept????
+type Action = "Requested" | "Viewed" | "Processing" | "Delivering" | "Delivered";
+
+// some werid socercry happening here
+const Timestamp: [Number, Action] = [Date.now(), "Requested"];
+
+
+
 const RequestSchema = new Schema({
     day_requested: {type: Date, required: true, default: Date.now() },
-    day_completed: {type: Date || undefined, required: false, defualt: undefined }
+    day_completed: {type: Date || undefined, required: false, defualt: undefined },
+    items: {
+        ribbons: { type: Array<String>, required: false, default: [] }
+    },
+    status: { type: String, required: true, default: "Requested", enum: ["Requested", "Viewed", "Processing", "Delivering", "Delivered"]},
+    history: { type: typeof Timestamp/* I CAST FIREBALL */, required: true, default: [[Date.now(),"Requested"]]}
 })
 // we making it pural bc express alr uses the name bruh
 const Requests = DashboardDB.model("Request",RequestSchema,"Requests")
